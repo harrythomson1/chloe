4ce5d5081189399402b839c41f170cbf4f6be359
Harry Thomson <haroldt95@hotmail.com>
2026-04-20 15:38:47 +0700

    Add and run rubocop on repo

--- test/models/user_condition_test.rb
+++ test/models/user_condition_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class UserConditionTest < ActiveSupport::TestCase
   # test "the truth" do
--- test/models/user_medication_test.rb
+++ test/models/user_medication_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class UserMedicationTest < ActiveSupport::TestCase
   # test "the truth" do
