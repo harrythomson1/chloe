15a35bc00c5e09e2fa5acefeabc37b0347974d3c
Harry Thomson <haroldt95@hotmail.com>
2026-04-23 15:54:57 +0700

    Render the search component on the conditions page

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,7 +1,11 @@
 class ConditionsController < ApplicationController
   before_action :authenticate_user!
 
-  def search
-    @conditions = Condition.where('name ILIKE ?', "%#{params[:query]}%")
+  def index
+    @user_conditions = current_user.conditions
+
+    return if params[:query].blank?
+
+    @search_results = Condition.where('name ILIKE ?', "%#{params[:query]}%")
   end
 end
