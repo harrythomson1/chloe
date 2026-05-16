152c587cf96bf64e3c674a5462a52eca544ebb5b
Harry Thomson <haroldt95@hotmail.com>
2026-05-16 16:17:36 +0700

    Add tests the confirm we add published conditions

--- app/services/nhs/sync_service.rb
+++ app/services/nhs/sync_service.rb
@@ -1,9 +1,39 @@
 module Nhs
   class SyncService
-    def self.call
-      url = 'https://api.service.nhs.uk/nhs-website-content/conditions/'
-      Faraday.get(url) do |req|
-        req.headers['apikey'] = Rails.application.credentials.nhs[:api_key]
+    class << self
+      def call
+        url = 'https://api.service.nhs.uk/nhs-website-content/conditions/'
+        while url
+          parsed = fetch(url)
+          process(parsed['significantLink'])
+          url = next_page_url(parsed['relatedLink'])
+        end
+      end
+
+      private
+
+      def fetch(url)
+        response = Faraday.get(url) do |req|
+          req.headers['apikey'] = Rails.application.credentials.nhs[:api_key]
+        end
+        JSON.parse(response.body)
+      end
+
+      def process(conditions)
+        conditions.each do |condition|
+          next unless condition['articleStatus'] == 'published'
+
+          Condition.create!(
+            name: condition['name'],
+            description: condition['description'],
+            source_url: condition['url'],
+            slug: condition['url'].split('/').compact_blank.last
+          )
+        end
+      end
+
+      def next_page_url(related_links)
+        related_links.find { |link| link['name'] == 'Next Page' }&.[]('url')
       end
     end
   end
--- spec/services/nhs/sync_service_spec.rb
+++ spec/services/nhs/sync_service_spec.rb
@@ -13,5 +13,22 @@ RSpec.describe Nhs::SyncService do
 
       expect(a_request(:get, 'https://api.service.nhs.uk/nhs-website-content/conditions/')).to have_been_made
     end
+
+    it 'adds published conditions to the database' do
+      stub_request(:get, 'https://api.service.nhs.uk/nhs-website-content/conditions/')
+        .to_return(
+          status: 200,
+          body: Rails.root.join('spec/fixtures/nhs_conditions_response_page_1.json').read,
+          headers: { 'Content-Type' => 'application/json' }
+        )
+      stub_request(:get, 'https://api.service.nhs.uk/nhs-website-content/conditions/?page=2')
+        .to_return(
+          status: 200,
+          body: Rails.root.join('spec/fixtures/nhs_conditions_response_page_2.json').read,
+          headers: { 'Content-Type' => 'application/json' }
+        )
+      described_class.call
+      expect(Condition.count).to eq(2)
+    end
   end
 end
