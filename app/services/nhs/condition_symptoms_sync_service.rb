5d38bb8134e01c6a791afee4081f6abc3de38dae
Harry Thomson <haroldt95@hotmail.com>
2026-05-17 12:52:50 +0700

    Allow the ConditionSymptomsSyncService to iterate over conditions

--- app/services/nhs/condition_symptoms_sync_service.rb
+++ app/services/nhs/condition_symptoms_sync_service.rb
@@ -1,9 +1,11 @@
 module Nhs
   class ConditionSymptomsSyncService
     def self.call
-      Faraday.new(request: { timeout: 60 }) do |f|
-        f.headers['apikey'] = Rails.application.credentials.nhs[:api_key]
-      end.get('https://api.service.nhs.uk/nhs-website-content/conditions/acanthosis-nigricans/')
+      Condition.find_each do |condition|
+        Faraday.new(request: { timeout: 60 }) do |f|
+          f.headers['apikey'] = Rails.application.credentials.nhs[:api_key]
+        end.get(condition.source_url)
+      end
     end
   end
 end
--- spec/services/nhs/condition_symptoms_sync_service_spec.rb
+++ spec/services/nhs/condition_symptoms_sync_service_spec.rb
@@ -14,5 +14,26 @@ RSpec.describe Nhs::ConditionSymptomsSyncService do
 
       expect(a_request(:get, condition.source_url)).to have_been_made
     end
+
+    it 'iterates through all conditions in the database' do
+      condition2 = create(:condition, name: 'Asthma', source_url: 'https://api.service.nhs.uk/nhs-website-content/conditions/asthma/')
+      stub_request(:get, condition.source_url)
+        .to_return(
+          status: 200,
+          body: { 'hasPart' => [] }.to_json,
+          headers: { 'Content-Type' => 'application/json' }
+        )
+      stub_request(:get, condition2.source_url)
+        .to_return(
+          status: 200,
+          body: { 'hasPart' => [] }.to_json,
+          headers: { 'Content-Type' => 'application/json' }
+        )
+
+      described_class.call
+
+      expect(a_request(:get, condition.source_url)).to have_been_made
+      expect(a_request(:get, condition2.source_url)).to have_been_made
+    end
   end
 end
