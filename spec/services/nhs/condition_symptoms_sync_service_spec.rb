6bb42b78fd389f5d1af565bcf439c39da12a529f
Harry Thomson <haroldt95@hotmail.com>
2026-05-17 13:15:54 +0700

    Extract stub to a before condition

--- spec/services/nhs/condition_symptoms_sync_service_spec.rb
+++ spec/services/nhs/condition_symptoms_sync_service_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 RSpec.describe Nhs::ConditionSymptomsSyncService do
   describe '.call' do
     let!(:condition) { create(:condition) }
+    before do
+      stub_request(:get, condition.source_url)
+        .to_return(
+          status: 200,
+          body: { 'hasPart' => [] }.to_json,
+          headers: { 'Content-Type' => 'application/json' }
+        )
+    end
     it 'makes a request to the NHS API' do
       stub_request(:get, condition.source_url)
         .to_return(
