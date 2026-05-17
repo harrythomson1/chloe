9b2e8fe4f3a352f02325f9333f84b99db4f1c193
Harry Thomson <haroldt95@hotmail.com>
2026-05-17 12:45:00 +0700

    Make the ConditionSymptomsSyncService call for one specific illness

--- app/services/nhs/condition_symptoms_sync_service.rb
+++ app/services/nhs/condition_symptoms_sync_service.rb
@@ -1,7 +1,9 @@
 module Nhs
   class ConditionSymptomsSyncService
     def self.call
-      nil
+      Faraday.new(request: { timeout: 60 }) do |f|
+        f.headers['apikey'] = Rails.application.credentials.nhs[:api_key]
+      end.get('https://api.service.nhs.uk/nhs-website-content/conditions/acanthosis-nigricans/')
     end
   end
 end
