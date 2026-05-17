4061da3effe73ff8fb4716d69821d70fda79489c
Harry Thomson <haroldt95@hotmail.com>
2026-05-17 16:45:46 +0700

    Add error handling and extrac function to a new method

--- app/services/nhs/condition_symptoms_sync_service.rb
+++ app/services/nhs/condition_symptoms_sync_service.rb
@@ -7,11 +7,20 @@ module Nhs
         client = connection
 
         Condition.find_each do |condition|
-          response = client.get(condition.source_url)
-          symptoms_section = JSON.parse(response.body)['hasPart'].find { |part| part['hasHealthAspect'] == 'http://schema.org/SymptomsHealthAspect' }
-          symptom = symptoms_section&.[]('description')
-          condition.update!(symptoms: symptom) if symptom
+          find_and_update_condition(condition, client)
+          sleep(0.5)
+        rescue StandardError => e
+          Rails.logger.error("Failed to sync symptoms for #{condition.name}: #{e.message}")
         end
+      end
+
+      private
+
+      def find_and_update_condition(condition, client)
+        response = client.get(condition.source_url)
+        symptoms_section = JSON.parse(response.body)['hasPart'].find { |part| part['hasHealthAspect'] == 'http://schema.org/SymptomsHealthAspect' }
+        symptom = symptoms_section&.[]('description')
+        condition.update!(symptoms: symptom) if symptom
       end
     end
   end
