3d3a84648c3e5642d2dca7e1643872914fd65e46
Harry Thomson <haroldt95@hotmail.com>
2026-05-21 13:46:19 +0700

    Congfigure faraday connection in ollama to parse as JSON

--- app/services/ollama/api_client.rb
+++ app/services/ollama/api_client.rb
@@ -1,7 +1,10 @@
 module Ollama
   module ApiClient
     def connection
-      Faraday.new(url: ENV.fetch('OLLAMA_HOST', 'http://localhost:11434'), request: { timeout: 300 })
+      Faraday.new(url: ENV.fetch('OLLAMA_HOST', 'http://localhost:11434'), request: { timeout: 300 }) do |f|
+        f.request :json
+        f.response :json
+      end
     end
   end
 end
