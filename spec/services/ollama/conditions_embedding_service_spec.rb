588a0a0746b6c41923629c9d2796e7beaf3e5e07
Harry Thomson <haroldt95@hotmail.com>
2026-05-23 10:24:54 +0700

    Wrap the specs in a describe block

--- spec/services/ollama/conditions_embedding_service_spec.rb
+++ spec/services/ollama/conditions_embedding_service_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Ollama::ConditionsEmbeddingService do
-  it 'makes a call to the embedding service' do
-    allow(Ollama::EmbeddingService).to receive(:call)
-    described_class.call
-    expect(Ollama::EmbeddingService).to have_received(:call)
+  describe '.call' do
+    it 'makes a call to the embedding service' do
+      allow(Ollama::EmbeddingService).to receive(:call)
+      described_class.call
+      expect(Ollama::EmbeddingService).to have_received(:call)
+    end
   end
 end
